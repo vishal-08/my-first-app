c93b73c12e8e9f93d4373d58ad39f6581ec43826
vishal-08 <66135224+vishal-08@users.noreply.github.com>
2021-10-08 15:15:12 +0530

    two way binding done

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false
   onServerCreation = "no server added"
-  serverName = ''
+  serverName = 'Transport'
 
   constructor() { 
     setTimeout(() => {
