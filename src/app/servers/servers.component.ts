db87bf566d1d65984693c5ea042fdaae00d56340
vishal-08 <66135224+vishal-08@users.noreply.github.com>
2021-10-08 16:23:17 +0530

    using ngIf else

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,39 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  // allowNewServer = false
-  // onServerCreation = "no server added"
-  // serverName = 'Transport'
+  allowNewServer = false
+  onServerCreation = "no server added"
+  serverName = 'Transport'
+  serverCreated = false
 
-  // constructor() { 
-  //   setTimeout(() => {
-  //     this.allowNewServer = true
-  //   }, 2000);
-
-  // }
-
-  // onCreateServer(){
-  //   this.onServerCreation = `server ${this.serverName} added successfully`
-  // };
-
-  // onUpdateServerName(event : Event){
-  //   this.serverName = (<HTMLInputElement>event.target).value
-  // }
-
-
-  allowInputTag = false
-  prefixValue = "My Name is "
-  name = "Vishal"
-
-
-  enableInput(){
-    this.allowInputTag = true
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true
+    }, 2000);
 
   }
 
-  onSubmit(){
+  onCreateServer(){
+    this.serverCreated = true
+    // this.onServerCreation = `server ${this.serverName} added successfully`
+  };
 
+  onUpdateServerName(event : Event){
+    this.serverName = (<HTMLInputElement>event.target).value
   }
+
+
+
 
   ngOnInit(): void {
   }
