5e7c1cf4d572a941ae8448855f814bd44bf2d88f
vishal-08 <66135224+vishal-08@users.noreply.github.com>
2021-10-08 17:28:24 +0530

    Exercise directive done

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,31 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  allowNewServer = false
-  onServerCreation = "no server added"
-  serverName = 'Transport'
-  serverCreated = false
-  servers = ['server 1', 'server 2']
+  // allowNewServer = false
+  // onServerCreation = "no server added"
+  // serverName = 'Transport'
+  // serverCreated = false
+  // servers = ['server 1', 'server 2']
 
-  constructor() { 
-    setTimeout(() => {
-      this.allowNewServer = true
-    }, 2000);
+  // constructor() { 
+  //   setTimeout(() => {
+  //     this.allowNewServer = true
+  //   }, 2000);
 
+  // }
+
+  // onCreateServer(){
+  //   this.serverCreated = true
+  //   this.servers.push(this.serverName)
+  //   // this.onServerCreation = `server ${this.serverName} added successfully`
+  // };
+
+  // onUpdateServerName(event : Event){
+  //   this.serverName = (<HTMLInputElement>event.target).value
+  // }
+
+
+  showDetail = false
+  log = [1];
+
+  toggleDisplay(){
+    this.showDetail = !this.showDetail
+    this.log.push(this.log.length + 1)
   }
-
-  onCreateServer(){
-    this.serverCreated = true
-    this.servers.push(this.serverName)
-    // this.onServerCreation = `server ${this.serverName} added successfully`
-  };
-
-  onUpdateServerName(event : Event){
-    this.serverName = (<HTMLInputElement>event.target).value
-  }
-
-
-
 
   ngOnInit(): void {
   }
