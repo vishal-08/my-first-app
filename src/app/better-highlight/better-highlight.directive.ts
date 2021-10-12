9a85a7ed5badcab6d237e11c73f251b46adf6dd5
vishal-08 <66135224+vishal-08@users.noreply.github.com>
2021-10-12 11:35:32 +0530

    changing event using hostbinding

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -7,15 +7,19 @@ export class BetterHighlightDirective  implements OnInit{
 
   constructor(private elementref : ElementRef, private renderer : Renderer2) { }
 
+  @HostBinding ('style.backgroundColor') backgroundColor : string = 'transparent'
+
   ngOnInit(){
-    
+   
   }
 
   @HostListener('mouseenter') mouseover(dataEvent : Event){
-    this.renderer.setStyle(this.elementref.nativeElement, 'backgroundColor', 'blue')
+    // this.renderer.setStyle(this.elementref.nativeElement, 'backgroundColor', 'blue')
+    this.backgroundColor = 'blue'
   }
 
   @HostListener('mouseleave') mouseleave(dataEvent : Event){
-    this.renderer.setStyle(this.elementref.nativeElement, 'backgroundColor', 'transparent')
+    //this.renderer.setStyle(this.elementref.nativeElement, 'backgroundColor', 'transparent')
+    this.backgroundColor = 'transparent'
   }
 }
