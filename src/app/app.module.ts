0afe8c945017f0c9c6c95cd49ac3105db246ef4e
vishal-08 <66135224+vishal-08@users.noreply.github.com>
2021-10-12 10:39:35 +0530

    create directives manually

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-  
+    BasicHighlightDirective
   
   ],
   imports: [
