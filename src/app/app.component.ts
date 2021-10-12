cc3237a33959ff5537539479007ae4b73746f5f9
vishal-08 <66135224+vishal-08@users.noreply.github.com>
2021-10-12 15:54:35 +0530

    logging service created and injected

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  odd = [1,3,5,7]
-  even = [2,4,6,8]
-  onlyOdd = false
-  value = 10
+  accounts = [
+    {
+      name: 'Master Account',
+      status: 'active'
+    },
+    {
+      name: 'Testaccount',
+      status: 'inactive'
+    },
+    {
+      name: 'Hidden Account',
+      status: 'unknown'
+    }
+  ];
+
+  onAccountAdded(newAccount: {name: string, status: string}) {
+    this.accounts.push(newAccount);
   }
 
+  onStatusChanged(updateInfo: {id: number, newStatus: string}) {
+    this.accounts[updateInfo.id].status = updateInfo.newStatus;
+  }
+}
