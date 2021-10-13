5af7cfe6c916858eb790a0ccbe8458765db30215
vishal-08 <66135224+vishal-08@users.noreply.github.com>
2021-10-13 17:50:26 +0530

    adding edit button and navigate edit component

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -11,7 +11,7 @@ import { ServersService } from '../servers.service';
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService, private route:ActivatedRoute) { }
+  constructor(private serversService: ServersService, private route:ActivatedRoute, private router : Router) { }
 
   ngOnInit() {
     const id = +this.route.snapshot.params['id']
@@ -19,6 +19,10 @@ export class ServerComponent implements OnInit {
     this.route.params.subscribe((param : Params) => {
       this.server = this.serversService.getServer(+param['id'])!
     })
+  }
+
+  editServer(){
+    this.router.navigate(['edit'],{relativeTo: this.route})
   }
 
 }
