08b2229f3719400bf2105dcded48286bb308e791
vishal-08 <66135224+vishal-08@users.noreply.github.com>
2021-10-13 15:58:48 +0530

    passing queryparameter and framents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,8 @@ const appRoute : Routes = [
   { path : '' , component : HomeComponent},
   { path : 'users' , component : UsersComponent},
   { path : 'users/:id/:name' , component : UserComponent},
-  { path : 'servers' , component : ServersComponent}
+  { path : 'servers' , component : ServersComponent},
+  { path : 'servers/:id/edit' , component : EditServerComponent}
 ]
 
 
