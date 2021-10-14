fcbe16da588a0ee46a87401fc63635d24c7970c8
vishal-08 <66135224+vishal-08@users.noreply.github.com>
2021-10-14 12:35:57 +0530

    error page component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,20 +11,11 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import {  RouterModule, Routes } from '@angular/router';
 
-const appRoute : Routes = [
-  { path : '' , component : HomeComponent},
-  { path : 'users' , component : UsersComponent , children : [
-    { path : ':id/:name' , component : UserComponent},
-  ]},
-  
-  { path : 'servers' , component : ServersComponent , children : [  
-    { path : ':id', component: ServerComponent},
-  { path : ':id/edit' , component : EditServerComponent}
-  ]},
-  
-]
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
+
+
 
 
 @NgModule({
@@ -35,12 +26,13 @@ const appRoute : Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoute)
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
